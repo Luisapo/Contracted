2ba484b833ff40f85c1adf7479a75b2057519bf1
Luisapo <luis.javier.apodaca@gmail.com>
2024-01-16 22:02:29 -0700

    Add files via upload

--- table.js
+++ table.js
@@ -6,24 +6,34 @@ document.addEventListener('DOMContentLoaded', () => {
           const tbody = document.createElement('tbody'); // Create a tbody element
           dataTable.style.borderSpacing = "2px";
           dataTable.style.borderCollapse = "separate";
+          tbody.style.display = "grid";                    
 
           // Populate table with data
-          data.forEach(rowData => {
-              const row = document.createElement('tr');
-              
-              Object.values(rowData).forEach(value => {
-                  const cell = document.createElement('td');                  
-                  cell.textContent = value;
-                  cell.style.border = "1px solid";                  
-                  cell.style.padding = "10px 20px";
-                  
-                  
-                  row.appendChild(cell);
-              });
-              tbody.appendChild(row); // Append rows to the tbody
+          data.forEach((rowData, rowIndex) => {
+            const row = document.createElement('tr');
+            row.style.display = 'grid';
+            row.style.gridAutoFlow = 'column';
+            row.style.gridTemplateColumns = '1fr 1fr 3fr 0.5fr 1fr';
+    
+            // Add a class to each row based on its index
+            row.classList.add(rowIndex % 2 === 0 ? 'even-row' : 'odd-row');
+    
+            Object.values(rowData).forEach((value, colIndex) => {
+              const cell = document.createElement('td');
+              cell.textContent = value;
+              cell.style.border = '1px solid';
+              cell.style.padding = '20px 20px';
+              cell.style.height = '100%'
+              cell.style.alignSelf = 'center';
+              cell.style.gridColumn = colIndex + 1;
+    
+              row.appendChild(cell);
+            });
+            tbody.appendChild(row);
           });
-
-          dataTable.appendChild(tbody); // Append the tbody to the table
+    
+          dataTable.appendChild(tbody);
+    
 
           // Add event listeners to the filter input elements
           const filterInputs = document.querySelectorAll('th input');
@@ -41,7 +51,14 @@ function filterTable() {
   const filters = [...query('th input')].map(e => new RegExp(e.value, 'i'));
 
   query('tbody tr').forEach(row => row.style.display = 
-    filters.every((f, i) => f.test(row.cells[i].textContent)) ? '' : 'none');
+    filters.every((f, i) => f.test(row.cells[i].textContent)) ? 'grid' : 'none');
+
+    const visibleRows = [...query('tbody tr')].filter(row => row.style.display !== 'none');
+    visibleRows.forEach((row, index) => {
+      row.classList.remove('even-row', 'odd-row');
+      row.classList.add(index % 2 === 0 ? 'even-row' : 'odd-row');
+    });
+
 }
 
 
